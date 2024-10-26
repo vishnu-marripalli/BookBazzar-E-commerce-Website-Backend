49560e88ab1b1bff4ff1e6653bec86e329b4b273
vishnu-marripalli <marripallivishnuvardhan@gmail.com>
2024-10-26 09:35:25 +0530

    bug fix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     const token =
     req.cookies?.accessToken ||
     req.header('Authorization')?.replace('Bearer','');
-    console.log(token)
+   
     if (!token) {
         throw new ApiError(401, 'Unauthorized request');
     }
